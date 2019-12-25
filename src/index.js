3da64e944e9921b5225315666f1ff602bdf72c1e
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-12-25 12:44:05 +0530

    Revert "Added middleware"
    This reverts commit c5c8d5ce39182b8d136b81165d651bc3d0ec3b8c.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reducer from './store/reducer';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-const logAction = store => {
-    return next => {
-        return action => {
-            const result = next(action);
-            console.log(`Caught in the middleware : ${JSON.stringify(result)}`);
-            return result;
-        }
-    }
-}
-const store = createStore(reducer, applyMiddleware(logAction));
+const store = createStore(reducer);
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
