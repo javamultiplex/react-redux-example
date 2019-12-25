d128e10ad380698c87d3caee99a6124c32fa121a
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-12-25 13:26:13 +0530

    Added saga

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reducer from './store/reducer';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
 import './index.css';
 import App from './App';
+import {watchAgeUp} from './sagas/saga';
 import * as serviceWorker from './serviceWorker';
-const store = createStore(reducer);
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+sagaMiddleware.run(watchAgeUp);
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,7 +5,7 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case "AGE_UP":
+        case "AGE_UP_ASYNC":
             return {
                 ...state,
                 age: state.age + action.value,
